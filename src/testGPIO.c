7c3c227ac5da29baccc6a914bbdadba0de90554d
Winnguyen1511 <khoanguyen1507dn@gmail.com>
2020-02-02 16:09:59 +0700

    debug from Pi

--- src/testGPIO.c
+++ src/testGPIO.c
@@ -6,6 +6,7 @@ int main(int argc, char** argv)
 {
     gpio_value_t tmpVal;
     edge_t  tmpEd;
+    direction_t tmpDir;
 
     printf("Welcome to test GPIO program:\n");
     // if(ioctl_is_exported(17) == FALSE)
@@ -14,19 +15,21 @@ int main(int argc, char** argv)
     // ioctl_cmd_set_active_low(17, HIGH_ACTIVE);
     // ioctl_cmd_set_value(17, HIGH);
     GPIO_t gpio17;
-    GPIO_Init_Custom(&gpio17, 17, INPUT, HIGH_ACTIVE,RISING, LOW);
+    GPIO_Init_Custom(&gpio17, 17, INPUT, HIGH_ACTIVE,FALLING, LOW);
     //ioctl_cmd(&gpio17, EXPORT_CMD, NULL);
     sleep(2);
     GPIO_get_value(&gpio17, &tmpVal);
     GPIO_get_edge(&gpio17, &tmpEd);
-    printf("Value=%d, Edge=%d\n", tmpVal, tmpEd);
+    GPIO_get_direction(&gpio17, &tmpDir);
+    printf("Value=%d, Edge=%d, Dir=%d\n", tmpVal, tmpEd, tmpDir);
     //printf("Info of gpio%d:\n",gpio17.gpio_num);
     //printf("num=%d, active_low=%d, dir=%d, value=%d\n",\
 	//         gpio17.gpio_num, gpio17.active_low, gpio17.direction, gpio17.value);
     sleep(2);
     GPIO_get_value(&gpio17, &tmpVal);
     GPIO_get_edge(&gpio17, &tmpEd);
-    printf("Value=%d, Edge=%d\n", tmpVal, tmpEd);
+    GPIO_get_direction(&gpio17, &tmpDir);
+    printf("Value=%d, Edge=%d, Dir=%d\n", tmpVal, tmpEd, tmpDir);
     // printf("num=%d, active_low=%d, dir=%d, value=%d\n",\
 	//         gpio17.gpio_num, gpio17.active_low, gpio17.direction, gpio17.value);
     // sleep(2);
