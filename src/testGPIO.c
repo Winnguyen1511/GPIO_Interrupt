0432d9b49af554829e271fdf861a2a9ce63d9b48
Winnguyen <khoanguyen1507dn@gmail.com>
2020-02-02 19:38:54 +0700

    debug

--- src/testGPIO.c
+++ src/testGPIO.c
@@ -32,6 +32,7 @@ int main(int argc, char** argv)
     GPIO_get_edge(&gpio17, &tmpEd);
     GPIO_get_direction(&gpio17, &tmpDir);
     printf("Value=%d, Edge=%d, Dir=%d\n", tmpVal, tmpEd, tmpDir);
+    sleep(2);
     // printf("num=%d, active_low=%d, dir=%d, value=%d\n",\
 	//         gpio17.gpio_num, gpio17.active_low, gpio17.direction, gpio17.value);
     // sleep(2);
@@ -58,6 +59,7 @@ int main(int argc, char** argv)
             GPIO_get_value(&gpio17, &val);
             printf("interrupted %d\n", val);
         }
+        fflush(stdout);
     }
     GPIO_Denit(&gpio17);
     return SUCCESS;
